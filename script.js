3ab64e68c12d0fd609ea08cd943294a379e3cdbe
Caio <caiob1305@gmail.com>
2022-10-17 13:51:52 -0300

    EnvioEntrevista

---
+++ script.js
@@ -0,0 +1,15 @@
+let idade =  parseInt(prompt("digite sua idade")) 
+
+if(idade >= 60){
+    alert("Você está autorizado a comprar o ingresso com 50% de desconto.")
+}
+
+else if(idade >= 18){
+    alert("Você está autorizado a comprar.")
+}
+
+else{
+    alert("Você não está autorizado a comprar o ingresso.")
+}
+
+
